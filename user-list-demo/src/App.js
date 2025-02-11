fa9303e144dbe290b1b73fc255c01ee1cc1c4a1d
Simon Fred Lubambo <slubambo57@gmail.com>
2025-02-11 10:57:56 +0300

    🍕 sliced a pie with initial code for SOLID Principles
    We are using React and an open api

---
+++ user-list-demo/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import UserList from "./components/userList";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React User Management</h1>
+      <UserList />
+    </div>
+  );
+}
+
+export default App;

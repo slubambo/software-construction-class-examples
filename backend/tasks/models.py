1b704acc47974142ff5d3113e31ec4d878a871ed
Simon Fred Lubambo <slubambo57@gmail.com>
2025-02-25 08:08:38 +0300

    🚧 new projects and other examples

---
+++ backend/tasks/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from users.models import CustomUser
+
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    assigned_to = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
